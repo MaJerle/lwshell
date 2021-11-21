e39db64c7b26fc2a5843963f028e02035d9d7894
Tilen Majerle <tilen.majerle@gmail.com>
2021-11-21 18:39:33 +0100

    Add version 1.1

--- lwshell/src/include/lwshell/lwshell_opt.h
+++ lwshell/src/include/lwshell/lwshell_opt.h
@@ -29,7 +29,7 @@
  * This file is part of LwSHELL - Lightweight shell library.
  *
  * Author:          Tilen MAJERLE <tilen@majerle.eu>
- * Version:         v1.0.0
+ * Version:         v1.1.0
  */
 #ifndef LWSHELL_HDR_OPT_H
 #define LWSHELL_HDR_OPT_H
--- lwshell/src/lwshell/lwshell.c
+++ lwshell/src/lwshell/lwshell.c
@@ -29,7 +29,7 @@
  * This file is part of LwSHELL - Lightweight shell library.
  *
  * Author:          Tilen MAJERLE <tilen@majerle.eu>
- * Version:         v1.0.0
+ * Version:         v1.1.0
  */
 #include <string.h>
 #include "lwshell/lwshell.h"
