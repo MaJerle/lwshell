10d824946052525e09bd45de7c227de2a01a9cbc
Tilen Majerle <tilen@majerle.eu>
2024-03-19 21:34:25 +0100

    Update library CMake system, to support automatic creation of options file, if user doesn't provide one

--- lwshell/library.cmake
+++ lwshell/library.cmake
@@ -1,13 +1,18 @@
 # 
+# LIB_PREFIX: LWSHELL
+#
 # This file provides set of variables for end user
 # and also generates one (or more) libraries, that can be added to the project using target_link_libraries(...)
 #
 # Before this file is included to the root CMakeLists file (using include() function), user can set some variables:
 #
-# LWSHELL_OPTS_DIR: If defined, it should set the folder path where options file shall be generated.
+# LWSHELL_OPTS_FILE: If defined, it is the path to the user options file. If not defined, one will be generated for you automatically
 # LWSHELL_COMPILE_OPTIONS: If defined, it provide compiler options for generated library.
 # LWSHELL_COMPILE_DEFINITIONS: If defined, it provides "-D" definitions to the library build
 #
+
+# Custom include directory
+set(LWSHELL_CUSTOM_INC_DIR ${CMAKE_CURRENT_BINARY_DIR}/lib_inc)
 
 # Library core sources
 set(lwshell_core_SRCS 
@@ -17,6 +22,7 @@ set(lwshell_core_SRCS
 # Setup include directories
 set(lwshell_include_DIRS
     ${CMAKE_CURRENT_LIST_DIR}/src/include
+    ${LWSHELL_CUSTOM_INC_DIR}
 )
 
 # Register library to the system
@@ -26,7 +32,11 @@ target_include_directories(lwshell INTERFACE ${lwshell_include_DIRS})
 target_compile_options(lwshell PRIVATE ${LWSHELL_COMPILE_OPTIONS})
 target_compile_definitions(lwshell PRIVATE ${LWSHELL_COMPILE_DEFINITIONS})
 
-# Create config file
-if(DEFINED LWSHELL_OPTS_DIR AND NOT EXISTS ${LWSHELL_OPTS_DIR}/lwshell_opts.h)
-    configure_file(${CMAKE_CURRENT_LIST_DIR}/src/include/lwshell/lwshell_opts_template.h ${LWSHELL_OPTS_DIR}/lwshell_opts.h COPYONLY)
+# Create config file if user didn't provide one info himself
+if(NOT LWSHELL_OPTS_FILE)
+    message(STATUS "Using default lwshell_opts.h file")
+    set(LWSHELL_OPTS_FILE ${CMAKE_CURRENT_LIST_DIR}/src/include/lwshell/lwshell_opts_template.h)
+else()
+    message(STATUS "Using custom lwshell_opts.h file from ${LWSHELL_OPTS_FILE}")
 endif()
+configure_file(${LWSHELL_OPTS_FILE} ${LWSHELL_CUSTOM_INC_DIR}/lwshell_opts.h COPYONLY)
